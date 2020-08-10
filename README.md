9dd8ff490c61ea5874a4f3a1ceeda277d8808a54
All-Schuck-Up <55608123+All-Schuck-Up@users.noreply.github.com>
2020-08-10 10:56:14 -0700

    Production build

---
+++ README.md
@@ -0,0 +1,2 @@
+# MERN-Devconnect
+This is a MERN full stack web app created during Brad Traversy's MERN course on udemy.com.
---
+++ client/build/precache-manifest.98ad653fa58ebefa828955f447fb0776.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b68222cb907ead3b45a3f77f0eb0de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e295943d471989ede35",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "4867bbd63e13a13c2d6f",
+    "url": "/static/js/2.72570448.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.72570448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e295943d471989ede35",
+    "url": "/static/js/main.630bb9bf.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "5572838d351b66bf6a3350b6d8d23cb8",
+    "url": "/static/media/spinner.5572838d.gif"
+  }
+]);
